abd02a2508707837ef8027d19c3f50b80c901095
ashkan <143696916+netblag@users.noreply.github.com>
2025-06-02 15:01:20 -0400

    Add files via upload

---
+++ admin/includes/config.php
@@ -0,0 +1,16 @@
+<?php
+//making config as we need this everytime we can just use it through include_once
+//1st step for database php connection
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "web";
+
+//Before we can access data in the MySQL database, we need to be able to connect to the server i.e php
+$conn = new mysqli($serverName, $dBUsername, $dBPassword, $dBName);
+
+// Check connection
+if (!$conn) {
+    die("اتصال ناموفق بود: " . $conn->connect_error());
+}
+?>
